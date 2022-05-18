bc11bd97e5ba3252a7235a635dd18e96230c1f66
JabedH <87475551+JabedH@users.noreply.github.com>
2022-05-19 00:34:00 +0600

    setup nev bar

---
+++ src/Components/Pages/AddNewList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddNewList = () => {
+  return (
+    <div>
+      <h1>add new list</h1>
+    </div>
+  );
+};
+
+export default AddNewList;
---
+++ src/Components/Pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-3xl my font-bold underline">
+        Welcome to To Do List!
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
